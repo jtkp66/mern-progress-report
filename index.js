902c4c26610e5ecfc96dcf8e711ebeeb6d0d35c9
Jamie Pham <jtkp66@gmail.com>
2019-03-11 19:50:57 -0700

    Added serverside routes and create react app in client folder for frontend.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+// const db = require()
+
+// app.get('/', (req, res) => {
+//     res.send({ hi: 'there'});
+// });
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/admin', require('./routes/admin'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`server started on port ${PORT}`));
